a4df23dd140b2e6b1068f536c339a4f12832634b
edureka <edureka@localhost.localdomain>
2018-04-25 18:25:58 +0530

    KafkaWordCount

---
+++ KafkaWordCount/src/main/scala/kafkaWordCount.scala
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+
+object kafkaWordCount{
+
+	def main(args : Array[String]){
+
+	val Array(zkQuorum, group, topics, numThreads)=args
+	val sparkConf = new SparkConf().setAppName("Integration with kafka").setMaster("local[*]")
+	val ssc = new StreamingContext(sparkConf,Seconds(10))
+
+	ssc.checkpoint("checkpoint")
+
+      	val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+	val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+	val words = lines.flatMap(_.split(" "))
+	val wordCounts = words.map(x => (x,1L)).reduceByKey(_+_)
+	wordCounts.print()
+
+	ssc.start()
+	ssc.awaitTermination() 
+	}
+}
