c963abcdadd68fc688de73f2eaa32a704a141c81
edureka <edureka@localhost.localdomain>
2018-05-08 09:26:30 +0530

    sparkStreaming_checkpoint

---
+++ sparkStreaming_checkpoint/build.sbt
@@ -0,0 +1,6 @@
+name := "sparkStreaming_checkpoint"
+version := "1.0"
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.1"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.1.1"
---
+++ sparkStreaming_checkpoint/src/main/scala/WordCount.scala
@@ -0,0 +1,114 @@
+import org.apache.spark.{SparkConf,SparkContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.util.IntParam
+import org.apache.spark.util.LongAccumulator
+import org.apache.spark.broadcast.Broadcast
+import java.io.File
+import com.google.common.io.Files
+import java.nio.charset.Charset
+
+object wordBlackList{
+	private var instance: Broadcast[Seq[String]] = null
+
+	def getInstance(sc:SparkContext):Broadcast[Seq[String]] = {
+		
+		if(instance == null){
+			
+			synchronized{
+				if(instance == null){
+					val wordBlackList = Seq("a","b","c")
+					instance = sc.broadcast(wordBlackList)
+				}
+			}
+		}
+		instance
+	}
+}
+
+object DroppedWordsCounter {
+
+  @volatile private var instance: LongAccumulator = null
+
+  def getInstance(sc: SparkContext): LongAccumulator = {
+    if (instance == null) {
+      synchronized {
+        if (instance == null) {
+          instance = sc.longAccumulator("WordsInBlacklistCounter")
+        }
+      }
+    }
+    instance
+  }
+}
+
+object WordCount{
+
+	def createContext(ip: String, port: Int, outputPath: String, checkpointDirectory: String): StreamingContext ={
+	
+		println("Creating new Context")
+		val outputFile = new File(outputPath)
+
+		if(outputFile.exists())
+			outputFile.delete()
+
+		val sparkConf = new SparkConf().setAppName("RecoverableNetworkWordCount")
+
+		val ssc = new StreamingContext(sparkConf,Seconds(10))
+		ssc.checkpoint(checkpointDirectory)
+
+		val lines = ssc.socketTextStream(ip,port)
+		val words = lines.flatMap(_.split(" "))
+		val wordCounts = words.map((_, 1)).reduceByKey(_ + _)
+
+		wordCounts.foreachRDD{(rdd : RDD[(String,Int)],time:Time) =>
+
+			val blacklist = wordBlackList.getInstance(rdd.sparkContext)
+		        val droppedWordsCounter = DroppedWordsCounter.getInstance(rdd.sparkContext)
+
+			val counts = rdd.filter{ case(word,count) => 
+				if(blacklist.value.contains(word)){
+					droppedWordsCounter.add(count)
+					false
+				}else{
+					true
+				}	
+			}.collect().mkString("[",",","]")	
+			val output = s"Counts at time $time $counts"
+      			println(output)
+      			println(s"Dropped ${droppedWordsCounter.value} word(s) totally")
+      			println(s"Appending to ${outputFile.getAbsolutePath}")
+			Files.append(output + "\n", outputFile, Charset.defaultCharset())
+		}
+		ssc
+	}
+
+	def main(args : Array[String]){
+	
+		if(args.length < 4){
+	
+			System.err.println(s"Your arguments were ${args.mkString("[", ", ", "]")}")
+			System.err.println(
+        		"""
+          		|Usage: RecoverableNetworkWordCount <hostname> <port> <checkpoint-directory>
+          		|     <output-file>. <hostname> and <port> describe the TCP server that Spark
+          		|     Streaming would connect to receive data. <checkpoint-directory> directory to
+          		|     HDFS-compatible file system which checkpoint data <output-file> file to which the
+          		|     word counts will be appended
+          		|
+          		|In local mode, <master> should be 'local[n]' with n > 1
+          		|Both <checkpoint-directory> and <output-file> must be absolute paths
+        		""".stripMargin
+      			)
+			System.exit(1)
+		}
+
+		val Array(ip,port,checkpointDirectory,outputPath) = args
+		val ssc = StreamingContext.getOrCreate(checkpointDirectory,() => createContext(ip, port.toInt, outputPath, checkpointDirectory))
+
+		ssc.start()
+		ssc.awaitTermination()
+	
+	}
+}
